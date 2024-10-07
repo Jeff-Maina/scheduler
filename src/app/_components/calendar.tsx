ebc351c9bc19a8acc541ae0e9888585016d209ba
Jeff-Maina <devyoungblud@gmail.com>
2024-10-07 21:42:45 +0300

    initial commit

---
+++ src/app/_components/calendar.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import timeGridPlugin from "@fullcalendar/timegrid";
+
+const Calendar = () => {
+  return (
+    <div className="max-w-3xl">
+      <FullCalendar
+        plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
+        headerToolbar={{
+          left: "prev,next today",
+          center: "title",
+        }}
+        nowIndicator={true}
+        editable={true}
+        selectable={true}
+        selectMirror={true}
+        initialEvents={[
+          { title: "nice event", start: new Date(), resourceId: "a" },
+        ]}
+      />
+    </div>
+  );
+};
+
+export default Calendar;
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Calendar from "./_components/calendar";
+
+export default function Home() {
+  return (
+    <div>
+      <Calendar />
+    </div>
+  );
+}
