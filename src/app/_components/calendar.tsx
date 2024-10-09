6dca7e847d57d1bab5caed155b29b17b9a937b43
Jeff-Maina <devyoungblud@gmail.com>
2024-10-09 11:29:48 +0300

    feat: add date picker

--- src/app/_components/calendar.tsx
+++ src/app/_components/calendar.tsx
@@ -1,30 +1,64 @@
 "use client";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { format, startOfToday } from "date-fns";
+import { Dispatch, SetStateAction, useState } from "react";
 
-import FullCalendar from "@fullcalendar/react";
-import dayGridPlugin from "@fullcalendar/daygrid";
-import interactionPlugin from "@fullcalendar/interaction";
-import timeGridPlugin from "@fullcalendar/timegrid";
+const Scheduler = () => {
+  let today = startOfToday();
 
-const Calendar = () => {
+  const [date, setDate] = useState<Date | undefined>(today);
+
   return (
-    <div className="max-w-3xl">
-      <FullCalendar
-        plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
-        headerToolbar={{
-          left: "prev,next today",
-          center: "title",
-        }}
-        nowIndicator={true}
-        editable={true}
-        selectable={true}
-        selectMirror={true}
-        initialEvents={[
-          { title: "nice event", start: new Date(), resourceId: "a" },
-        ]}
-        allDayContent
-      />
+    <div className="max-w-7xl w-full h-[70vh] border border-neutral-200 rounded-md">
+      <div className="w-full h-16 border-b flex items-center p-4">
+        <CurrentDate date={date} setDate={setDate} />
+      </div>
     </div>
   );
 };
 
-export default Calendar;
+// ************************** MINI COMPONENTS ************************************** //
+
+const CurrentDate = ({
+  date,
+  setDate,
+}: {
+  date: Date | undefined;
+  setDate: Dispatch<SetStateAction<Date | undefined>>;
+}) => {
+  return (
+    <div>
+      {" "}
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button
+            variant={"ghost"}
+            className={cn(
+              " justify-start text-left font-semibold text-xl tracking-tight",
+              !date && "text-muted-foreground"
+            )}
+          >
+            {date ? format(date, "MMMM yyyy") : <span>Pick a date</span>}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-auto p-0" align="start">
+          <Calendar
+            mode="single"
+            selected={date}
+            onSelect={setDate}
+            initialFocus
+          />
+        </PopoverContent>
+      </Popover>
+    </div>
+  );
+};
+
+export default Scheduler;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,9 @@ import Calendar from "./_components/calendar";
 
 export default function Home() {
   return (
-    <div>
+    <div className="h-screen w-full flex items-center justify-center">
       <Calendar />
     </div>
   );
 }
+
