68cce400e0891ed0ba14ce18710b185e15b998a6
Jeff-Maina <devyoungblud@gmail.com>
2024-10-31 21:34:53 +0300

    refactor view selection ui

--- src/app/_components/select-view-menu.tsx
+++ src/app/_components/select-view-menu.tsx
@@ -16,77 +16,37 @@ type TSelectViewMenuProps = {
   changeView: (value: TView) => void;
 };
 
+const ViewsList: { view: TView; icon: React.ReactNode }[] = [
+  {
+    view: "day",
+    icon: <Calendar size={16} />,
+  },
+  {
+    view: "week",
+    icon: <Table2 size={16} />,
+  },
+  {
+    view: "month",
+    icon: <Grid3X3 size={16} />,
+  },
+];
+
 const SelectViewMenu = ({ selectedView, changeView }: TSelectViewMenuProps) => {
   return (
-    // <DropdownMenu>
-    //   <DropdownMenuTrigger asChild>
-    //     <Button
-    //       variant={"outline"}
-    //       size={'sm'}
-    //       className="flex bg-white items-center gap-1 border border-neutral-200 text-sm hover:bg-neutral-100 p-1 px-3  hover:text-black rounded"
-    //     >
-    //       <p className="capitalize">{selectedView}</p>
-    //       <ChevronDown size={14} strokeWidth={3} />
-    //     </Button>
-    //   </DropdownMenuTrigger>
-    //   <DropdownMenuContent align="end" className="rounded-md shadow-sm w-36">
-    //     <DropdownMenuCheckboxItem
-    //       checked={selectedView === "day"}
-    //       onCheckedChange={() => changeView("day")}
-    //       className="cursor-pointer data-[state='checked']:bg-neutral-200/70 text-neutral-800 hover:text-black"
-    //     >
-    //       Day
-    //     </DropdownMenuCheckboxItem>
-    //     <DropdownMenuCheckboxItem
-    //       checked={selectedView === "week"}
-    //       onCheckedChange={() => changeView("week")}
-    //       className="cursor-pointer data-[state='checked']:bg-neutral-200/70 text-neutral-800 hover:text-black"
-    //     >
-    //       Week
-    //     </DropdownMenuCheckboxItem>
-    //     <DropdownMenuCheckboxItem
-    //       checked={selectedView === "month"}
-    //       onCheckedChange={() => changeView("month")}
-    //       className="cursor-pointer data-[state='checked']:bg-neutral-200/70 text-neutral-800 hover:text-black"
-    //     >
-    //       Month
-    //     </DropdownMenuCheckboxItem>
-    //   </DropdownMenuContent>
-    // </DropdownMenu>
     <div className="h-9 border rounded flex items-center p-1">
-      <TooltipWrapper label="day view" className="font-semibold">
-        <button
-          onClick={() => changeView("day")}
-          className={cn(
-            "h-full px-3 rounded text-neutral-600 hover:bg-neutral-100 hover:text-black",
-            selectedView === "day" ? "!bg-neutral-300 !text-black" : ""
-          )}
-        >
-          <Calendar size={16} />
-        </button>
-      </TooltipWrapper>
-      <TooltipWrapper label="week view" className="font-semibold">
-        <button
-          onClick={() => changeView("week")}
-          className={cn(
-            "h-full px-3 rounded text-neutral-600 hover:bg-neutral-100 hover:text-black",
-            selectedView === "week" ? "!bg-neutral-300 !text-black" : ""
-          )}
-        >
-          <Table2 size={16} />
-        </button>
-      </TooltipWrapper>
-      <TooltipWrapper label="month view" className="font-semibold">
-        <button
-          onClick={() => changeView("month")}
-          className={cn(
-            "h-full px-3 rounded text-neutral-600 hover:bg-neutral-100 hover:text-black",
-            selectedView === "month" ? "!bg-neutral-300 !text-black" : ""
-          )}
-        >
-          <Grid3X3 size={16} />
-        </button>
-      </TooltipWrapper>
+      {ViewsList.map(({ view, icon }, index) => (
+        <TooltipWrapper label={`${view} view`} className="font-semibold">
+          <button
+            onClick={() => changeView(view)}
+            className={cn(
+              "h-full px-3 rounded text-neutral-600 hover:bg-neutral-100 hover:text-black",
+              selectedView === view ? "!bg-neutral-300 !text-black" : ""
+            )}
+          >
+            {icon}
+          </button>
+        </TooltipWrapper>
+      ))}
     </div>
   );
 };
